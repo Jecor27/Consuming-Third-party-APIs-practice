0b630ac4fd59e34ce8ff9daad38c2771c357298e
Jesus <jjcorrales76@gmail.com>
2025-02-26 13:56:12 -0700

    commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+function App() {
+  const [ships, setShips] = useState([]);
+
+  useEffect(() => {
+    async function getStarships() {
+      try {
+        let resp = await fetch("https://swapi.dev/api/starships");
+        const data = await resp.json();
+        console.log(data.results[0]);
+        setShips(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getStarships()
+  }, []);
+
+  
+  const output = ships.map((ship) => {
+    return <Box data={ship} key={ship.model} />;
+  });
+
+  return <div className={"container"}>{output}</div>;
+}
+
+export default App;
+
+function Box({ data }) {
+  return (
+    <>
+      <div className={"box"}>{data.name}</div>
+    </>
+  );
+}
